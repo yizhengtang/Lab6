830574f8e533994d3ce6023d3818867ce52a622e
Yi Zheng Tang <G00425075@atu.ie>
2023-11-06 16:32:40 +0000

    Created a code to assign values into array and print them out.

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -4,7 +4,21 @@ package ie.atu;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        int []anArray;
+        int i, num = 100;
 
+        anArray = new int [10];
+
+        for (i = 0; i < 10; i ++)
+        {
+            anArray[i] = num;
+            num+=100;
+        }
+
+        for (i = 0; i < 10 ; i ++)
+        {
+            System.out.println("Element in index " + i + ": " + anArray[i]);
+        }
 
     }
 }
